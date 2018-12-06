4eb74131855dffddbcd50e3a280cc3bd7f5ccac4
Leor <leor.fishman@gmail.com>
2018-12-06 08:50:06 -0500

    weight saving and loading

--- final project.py
+++ final project.py
@@ -1,5 +1,6 @@
 from dominiate import cards, dominion, game, players, combobot, derivbot
 from keras import models, layers, regularizers, preprocessing
+import csv
 
 canonical_order = [game.curse, game.estate, game.duchy, game.province, game.copper, game.silver, game.gold,
                    cards.village, cards.cellar, cards.smithy, cards.festival, cards.market, cards.laboratory,
@@ -21,15 +22,34 @@ def g2f (carddict):
 
 
 class combo_learner(combobot.ComboBot):
-    def __init__(self):
-        self.buy_weights = []
-        self.trash_weights = []
-        self.discard_weights = []
-        self.play_weights = []
+    def __init__(self, loadfile = None):
+        if loadfile is None:
+            self.buy_weights = [0 for _ in range((len(canonical_order) * 2))]
+            self.trash_weights = [0 for _ in range((len(canonical_order) * 3))]
+            self.discard_weights = [0 for _ in range((len(canonical_order) + 3))]
+            self.play_weights = [0 for _ in range((len(canonical_order) + 3))]
+            self.weights = [self.buy_weights, self.trash_weights, self.discard_weights, self.play_weights]
+        else:
+            self.loadweights(filename=loadfile)
+            self.weights = [self.buy_weights, self.trash_weights, self.discard_weights, self.play_weights]
 
     """
     4 separate q learners
     """
+    def loadweights(self, filename = "weights.csv"):
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            introws = [[int(r) for r in row] for row in reader]
+            self.buy_weights = introws[0]
+            self.trash_weights = introws[1]
+            self.discard_weights = introws[2]
+            self.play_weights = introws[3]
+    def saveweights(self, filename = "weights.csv"):
+        with open(filename, "w+") as file:
+            writer = csv.writer(file)
+            self.weights = [self.buy_weights, self.trash_weights, self.discard_weights, self.play_weights]
+            for weight in self.weights:
+                writer.writerow(weight)
     def from_state_features_buy (self, decision):
         game = decision.game
         deck = decision.state().all_cards()
