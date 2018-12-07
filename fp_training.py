def412b7d4bcd86e45a4194a12fc2f49a0bbdb25
Cate Pinto <catherinepinto@Cates-MacBook-Pro.local>
2018-12-06 23:38:14 -0500

    generalized

--- fp_training.py
+++ fp_training.py
@@ -2,40 +2,37 @@ from dominiate import cards, dominion, game, players, combobot, derivbot, basic_
 import csv
 from final_project import *
 
-# Options for Players:
+# Options for Players: players.BigMoney()
 # ComboLearner()            our q learner
+# RandomBot()               an agent that acts randomly
+# 
 # smithyComboBot()          from source code, basic ai
 # chapelComboBot()          from source code, basic ai
 # HillClimbBot(2, 3, 40)    from source code, basic ai
 # ... any more that we create
 
-def testRandomAgent(iterations):
-    playerR = basic_ai.GreedyBot()
-    player2 = players.BigMoney()
+def testNotQAgents(player1, player2, iterations):
     wins = [] # binary array tracking wins of each game
     for i in range(iterations):
-        board = game.Game.setup([playerR, player2], cards.variable_cards)
+        board = game.Game.setup([player1, player2], cards.variable_cards)
         results = board.run()    # returns a dictionary mapping players to scores
-        if results[0][0].name == "RandomBot":
-            scoreR = results[0][1]
+        if results[0][0].name == player1.name:
+            score1 = results[0][1]
             score2 = results[1][1]
         else:
-            scoreR = results[1][1]
+            score1 = results[1][1]
             score2 = results[0][1]
         
-        # 1=R wins, 0 otherwise
-        if scoreR >= score2:
+        # 1 if player 1 wins, 0 otherwise
+        if score1 > score2:
             wins.append(1)
         else:
             wins.append(0)
+    
+    # return winning percentage
     print(float(sum(wins)) / float(iterations))
     return float(sum(wins)) / float(iterations)
-    # return winning percentage
-
-
-        # win for R = 1, sum of array/trials = winning percentage
-
-
+    
 
 def testing(player1, player2, iterations):
     game_results = []
@@ -55,4 +52,5 @@ if __name__ == '__main__':
     #testing(ComboLearner(), ComboLearner(), 10)
     #testing(ComboLearner(), smithyComboBot(), 10)
     #testing(ComboLearner(), HillClimbBot(), 10)
-    testRandomAgent(100)
+    # testNotQAgents(players.BigMoney(), basic_ai.GreedyBot(), 10)
+    testNotQAgents(basic_ai.GreedyBot(), basic_ai.GreedyBot(), 1000)
