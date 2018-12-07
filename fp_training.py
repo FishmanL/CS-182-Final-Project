5e32607525c4ff519b609eb3cd07bf3f3371d445
Cate Pinto <catherinepinto@Cates-MacBook-Pro.local>
2018-12-06 23:23:30 -0500

    testing loop done

--- fp_training.py
+++ fp_training.py
@@ -10,13 +10,28 @@ from final_project import *
 # ... any more that we create
 
 def testRandomAgent(iterations):
-    playerR = basic_ai.RandomBot()
+    playerR = basic_ai.GreedyBot()
     player2 = players.BigMoney()
-    wins = [] # binary array tracking wins of each game; 1=R wins, 0 otherwise
+    wins = [] # binary array tracking wins of each game
     for i in range(iterations):
         board = game.Game.setup([playerR, player2], cards.variable_cards)
         results = board.run()    # returns a dictionary mapping players to scores
-        print(results)
+        if results[0][0].name == "RandomBot":
+            scoreR = results[0][1]
+            score2 = results[1][1]
+        else:
+            scoreR = results[1][1]
+            score2 = results[0][1]
+        
+        # 1=R wins, 0 otherwise
+        if scoreR >= score2:
+            wins.append(1)
+        else:
+            wins.append(0)
+    print(float(sum(wins)) / float(iterations))
+    return float(sum(wins)) / float(iterations)
+    # return winning percentage
+
 
         # win for R = 1, sum of array/trials = winning percentage
 
@@ -40,4 +55,4 @@ if __name__ == '__main__':
     #testing(ComboLearner(), ComboLearner(), 10)
     #testing(ComboLearner(), smithyComboBot(), 10)
     #testing(ComboLearner(), HillClimbBot(), 10)
-    testRandomAgent(10)
+    testRandomAgent(100)
