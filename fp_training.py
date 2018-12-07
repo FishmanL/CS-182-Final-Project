646d4079e62b82b79103db1ac763d59dca4c8033
Cate Pinto <catherinepinto@Cates-MacBook-Pro.local>
2018-12-06 22:30:16 -0500

    working on random testing loop

--- fp_training.py
+++ fp_training.py
@@ -1,12 +1,4 @@
-from game import *
-from players import *
-from basic_ai import *
-from combobot import *
-from cards import variable_cards
-from collections import defaultdict
-import random
-from dominiate import cards, dominion, game, players, combobot, derivbot
-from keras import models, layers, regularizers, preprocessing
+from dominiate import cards, dominion, game, players, combobot, derivbot, basic_ai
 import csv
 from final_project import *
 
@@ -17,22 +9,35 @@ from final_project import *
 # HillClimbBot(2, 3, 40)    from source code, basic ai
 # ... any more that we create
 
-def testing(player1, player2, player3, iterations):
+def testRandomAgent(iterations):
+    playerR = basic_ai.RandomBot()
+    player2 = players.BigMoney()
+    wins = [] # binary array tracking wins of each game; 1=R wins, 0 otherwise
+    for i in range(iterations):
+        board = game.Game.setup([playerR, player2], cards.variable_cards)
+        results = board.run()    # returns a dictionary mapping players to scores
+        print(results)
+
+        # win for R = 1, sum of array/trials = winning percentage
+
+
+
+def testing(player1, player2, iterations):
     game_results = []
 
     # play specified number of games
     for i in range(iterations):
-        game = Game.setup([player1, player2, player3], variable_cards)
+        game = Game.setup([player1, player2], variable_cards)
         results = game.run()
         player1.saveweights("test_player1.csv")
         player2.saveweights("test_player2.csv")
-        player3.saveweights("test_player3.csv")
         game_results.append(results)
 
     print(game_results)
     return game_results
 
 if __name__ == '__main__':
-    testing(ComboLearner(), ComboLearner(), ComboLearner(), 10)
-    testing(ComboLearner(), smithyComboBot(), chapelComboBot(), 10)
-    testing(ComboLearner(), HillClimbBot(), chapelComboBot(), 10)
+    #testing(ComboLearner(), ComboLearner(), 10)
+    #testing(ComboLearner(), smithyComboBot(), 10)
+    #testing(ComboLearner(), HillClimbBot(), 10)
+    testRandomAgent(10)
